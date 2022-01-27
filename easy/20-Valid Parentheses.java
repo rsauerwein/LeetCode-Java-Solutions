52344f519f683ca361e915a580661c1a99d19ce5
Rene Sauerwein <rene@sauerwein.cc>
2022-01-27 18:52:31 +0100

    add exercise

---
+++ easy/20-Valid Parentheses.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean isValid(String s) {
+        if (s.length() == 1) return false;
+
+        Map<Character, Character> mappings = new HashMap<>();
+        mappings.put(')', '(');
+        mappings.put(']', '[');
+        mappings.put('}', '{');
+
+        Stack<Character> prev = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (c == '(' || c == '[' || c == '{') {
+                prev.push(c);
+            } else {
+                if (prev.isEmpty()) return false;
+                char curr = prev.pop();
+                if (mappings.get(c) != curr) return false;
+            }
+        }
+        return prev.isEmpty();
+    }
+}
