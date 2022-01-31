c4e590bf7d8f5c9d29a5316a0c60563df6447340
Rene Sauerwein <rene.sauerwein@abacus.ch>
2022-01-31 15:47:51 +0100

    Performance optimizations

--- easy/53-Maximum Subarray.java
+++ easy/53-Maximum Subarray.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        if (nums.length == 1) return nums[0];
         int res = Integer.MIN_VALUE;
         int curr = 0;
         for (int num : nums) {
