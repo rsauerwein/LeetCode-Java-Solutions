cc21b7ae0baed68164b2cbe50b33406d0ee2a67b
Rene Sauerwein <rene.sauerwein@abacus.ch>
2022-01-31 15:13:53 +0100

    Add new exercise

---
+++ easy/53-Maximum Subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int res = Integer.MIN_VALUE;
+        int curr = 0;
+        for (int num : nums) {
+            curr += num;
+            res = Math.max(curr, res);
+            if (curr < 0) curr = 0;
+        }
+        return res;
+    }
+}
